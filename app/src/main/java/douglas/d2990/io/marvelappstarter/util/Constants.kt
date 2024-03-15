befd2cad07998c699b81c02d34db3b6ad7d396ae
Douglas <64422034+douglas2990@users.noreply.github.com>
2024-03-15 09:39:27 -0300

    Constants.kt

--- app/src/main/java/douglas/d2990/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/douglas/d2990/io/marvelappstarter/util/Constants.kt
@@ -4,8 +4,8 @@ object Constants {
 
     const val DATABASE_NAME = "marvel.db"
     const val BASE_URL = "https://gateway.marvel.com/v1/public/"
-    const val PUBLIC_KEY = "100da2c5369c2a6d9b0227b449b0c319"
-    const val PRIVATE_KEY = "036fcf030b91671dbafc49ee5c145e76eeca1513"
+    const val PUBLIC_KEY = "xxxxxxxxx9"
+    const val PRIVATE_KEY = "xxxxxxxxxxx3"
 
     const val LAST_SEARCH_QUERY = "Last_search_query"
     const val DEFAULT_QUERY = ""
